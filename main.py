e5f18610460495d2221936721dded02125c4b069
zyagaimooisii <127655232+zyagaimooisii@users.noreply.github.com>
2024-06-17 17:04:19 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+@client.event
+async def on_message(message):
+    emoji ="👍"
+    await message.add_reaction(emoji)
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+# Web サーバの立ち上げ
+keep_alive()
+client.run(TOKEN)
